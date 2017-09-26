93f48d3e83e1db668a2e0240bcc1914ae4ae5635
Eran Hammer <eran@hammer.io>
2017-09-25 22:54:45 -0700

    Node 8. Closes #18

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@
 const Crypto = require('crypto');
 const Stream = require('stream');
 const Util = require('util');
+
 const Lab = require('lab');
 const Wreck = require('wreck');
 const B64 = require('..');
@@ -23,102 +24,81 @@ const it = lab.it;
 const expect = Lab.expect;
 
 
-it('pipes buffer through encoder and decoder', (done) => {
+it('pipes buffer through encoder and decoder', async () => {
 
     const buffer = Crypto.randomBytes(1024);
-    internals.test(buffer, (err, payload) => {
-
-        expect(err).to.not.exist();
-        expect(payload).to.equal(buffer.toString());
-        done();
-    });
+    const payload = await internals.test(buffer);
+    expect(payload).to.equal(buffer.toString());
 });
 
 
 describe('decode()', () => {
 
-    it('decodes a short buffer (1)', (done) => {
+    it('decodes a short buffer (1)', async () => {
 
         const value = '0';
         const encoded = B64.encode(new Buffer(value));
         expect(B64.decode(encoded).toString()).to.equal(value);
-        done();
     });
 
-    it('decodes an incomplete buffer', (done) => {
+    it('decodes an incomplete buffer', async () => {
 
         const value = '';
         const encoded = new Buffer('A');
         expect(B64.decode(encoded).toString()).to.equal(value);
-        done();
     });
 
-    it('decodes an whitespace buffer', (done) => {
+    it('decodes an whitespace buffer', async () => {
 
         const value = '';
         const encoded = new Buffer('     ');
         expect(B64.decode(encoded).toString()).to.equal(value);
-        done();
     });
 
-    it('decodes a buffer with whitespace', (done) => {
+    it('decodes a buffer with whitespace', async () => {
 
         const value = '0123456789';
         const encoded = new Buffer('M  D\nEy\tMz\r\nQ1Nj\rc4\r\nO Q ==');
         expect(B64.decode(encoded).toString()).to.equal(value);
-        done();
     });
 
-    it('decodes a buffer with 4th invalid character', (done) => {
+    it('decodes a buffer with 4th invalid character', async () => {
 
         const value = '01';
         const encoded = new Buffer('MDE$');
         expect(B64.decode(encoded).toString()).to.equal(value);
-        done();
     });
 });
 
 
 describe('Encoder', () => {
 
-    it('process remainder', (done) => {
+    it('process remainder', async () => {
 
         const buffer = [Crypto.randomBytes(5), Crypto.randomBytes(5), Crypto.randomBytes(5), Crypto.randomBytes(5)];
-        internals.test(buffer, (err, payload) => {
-
-            expect(err).to.not.exist();
-            expect(payload).to.equal(Buffer.concat(buffer).toString());
-            done();
-        });
+        const payload = await internals.test(buffer);
+        expect(payload).to.equal(Buffer.concat(buffer).toString());
     });
 
-    it('flushes remainder', (done) => {
+    it('flushes remainder', async () => {
 
         const buffer = [Crypto.randomBytes(5), Crypto.randomBytes(5), Crypto.randomBytes(5), Crypto.randomBytes(1)];
-        internals.test(buffer, (err, payload) => {
-
-            expect(err).to.not.exist();
-            expect(payload).to.equal(Buffer.concat(buffer).toString());
-            done();
-        });
+        const payload = await internals.test(buffer);
+        expect(payload).to.equal(Buffer.concat(buffer).toString());
     });
 
-    it('skips empty remainder', (done) => {
+    it('skips empty remainder', async () => {
 
         const buffer = [Crypto.randomBytes(5), Crypto.randomBytes(5), Crypto.randomBytes(5), Crypto.randomBytes(3)];
-        internals.test(buffer, (err, payload) => {
-
-            expect(err).to.not.exist();
-            expect(payload).to.equal(Buffer.concat(buffer).toString());
-            done();
-        });
+        const payload = await internals.test(buffer);
+        expect(payload).to.equal(Buffer.concat(buffer).toString());
     });
 });
 
 
 describe('Decoder', () => {
 
-    it('process remainder', (done) => {
+    it('process remainder', async () => {
 
         const value = Crypto.randomBytes(100);
         const encoded = B64.encode(value);
@@ -126,15 +106,11 @@ describe('Decoder', () => {
         const stream = new internals.Payload([encoded.slice(0, 3), encoded.slice(3, 9), encoded.slice(9)]);
         const source = stream.pipe(new B64.Decoder());
 
-        Wreck.read(source, {}, (err, payload) => {
-
-            expect(err).to.not.exist();
-            expect(payload.toString()).to.equal(value.toString());
-            done();
-        });
+        const payload = await Wreck.read(source);
+        expect(payload.toString()).to.equal(value.toString());
     });
 
-    it('flushes remainder', (done) => {
+    it('flushes remainder', async () => {
 
         const value = '0123456789';
         const encoded = B64.encode(new Buffer(value));         // MDEyMzQ1Njc4OQ==
@@ -142,12 +118,8 @@ describe('Decoder', () => {
         const stream = new internals.Payload([encoded.slice(0, 14)]);
         const source = stream.pipe(new B64.Decoder());
 
-        Wreck.read(source, {}, (err, payload) => {
-
-            expect(err).to.not.exist();
-            expect(payload.toString()).to.equal(value.toString());
-            done();
-        });
+        const payload = await Wreck.read(source);
+        expect(payload.toString()).to.equal(value.toString());
     });
 });
 
@@ -175,13 +147,11 @@ internals.Payload.prototype._read = function (size) {
 };
 
 
-internals.test = function (buffer, callback) {
+internals.test = async function (buffer) {
 
     const stream = new internals.Payload(buffer);
     const source = stream.pipe(new B64.Encoder()).pipe(new B64.Decoder());
 
-    Wreck.read(source, {}, (err, payload) => {
-
-        callback(err, payload ? payload.toString() : null);
-    });
+    const payload = await Wreck.read(source);
+    return payload.toString();
 };
